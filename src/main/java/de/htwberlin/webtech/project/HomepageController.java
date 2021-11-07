93668c7f60447861c9af71535c03430e6ba640c8
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-07 09:23:52 +0100

    change "start" back to "greeting"

--- src/main/java/de/htwberlin/webtech/project/HomepageController.java
+++ src/main/java/de/htwberlin/webtech/project/HomepageController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HomepageController {
 
-    @GetMapping("/start")
+    @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="stitching fan") String name, Model model) {
         model.addAttribute("name", name);
-        return "start";
+        return "greeting";
     }
 
     @GetMapping("/add-entry")
