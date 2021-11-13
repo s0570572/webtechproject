cf3c16142c4c70f675e5581135e8a031ed5d8a29
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-13 16:50:18 +0100

    add changes to ci.yml

--- src/main/java/de/htwberlin/webtech/project/database/entry_ct.sql
+++ src/main/java/de/htwberlin/webtech/project/database/entry_ct.sql
@@ -1,3 +1,9 @@
 create table entry (
-    --add code later
+    EntryID     integer         not null,
+    Title       varchar(30)     not null,
+    Description varchar(250)    null,
+    Topic       varchar(15)     not null,
+    Difficulty  varchar(8)      not null,
+    Link        varchar(100)    not null,
+    constraint pk_entry primary key(EntryID)
 );
