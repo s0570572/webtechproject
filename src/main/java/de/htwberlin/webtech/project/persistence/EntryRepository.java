5081ddf7c3a490871635cd5703772091e2456a32
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-14 12:40:47 +0100

    expand repository class

--- src/main/java/de/htwberlin/webtech/project/persistence/EntryRepository.java
+++ src/main/java/de/htwberlin/webtech/project/persistence/EntryRepository.java
@@ -1,8 +1,19 @@
 package de.htwberlin.webtech.project.persistence;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface EntryRepository extends CrudRepository<EntryEntity, Long> {
+public interface EntryRepository extends JpaRepository<EntryEntity, Long> {
+
+    List<EntryEntity> findAllByDifficultyEntity(DifficultyEntity difficulty);
+
+    List<EntryEntity> findAllByTopicEntity(TopicEntity topic);
+
+    EntryEntity save(EntryEntity entry);
+
+    @Override
+    void delete(EntryEntity entity);
 }
