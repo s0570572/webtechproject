61e66917b938c179fa6eb0dcbbba0aa74fbab7fd
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-12-20 17:55:17 +0100

    move package to project

--- src/main/java/de/htwberlin/webtech/project/config/WebConfig.java
+++ src/main/java/de/htwberlin/webtech/project/config/WebConfig.java
@@ -1,4 +1,4 @@
-package de.htwberlin.webtech.config;
+package de.htwberlin.webtech.project.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
