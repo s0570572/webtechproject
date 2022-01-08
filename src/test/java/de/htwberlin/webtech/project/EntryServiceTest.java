719d1d8e9054b6741472ee3132c7967dd87fb14f
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2022-01-08 13:01:03 +0100

    add delete entity backend test, add mock to transform entity test

---
+++ src/test/java/de/htwberlin/webtech/project/EntryServiceTest.java
@@ -0,0 +1,64 @@
+package de.htwberlin.webtech.project;
+
+import de.htwberlin.webtech.project.persistence.EntryEntity;
+import de.htwberlin.webtech.project.persistence.EntryRepository;
+import de.htwberlin.webtech.project.service.EntryService;
+import de.htwberlin.webtech.project.web.Topic;
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class EntryServiceTest implements WithAssertions {
+
+    @Mock
+    private EntryRepository repo;
+
+    @InjectMocks
+    private EntryService underTest;
+
+    @Test
+    @DisplayName("should return true if delete was successful")
+    void should_return_true_if_delete_was_successful() {
+        // given
+        Long givenId = 3L;
+        doReturn(true).when(repo).existsById(givenId);
+
+        // when
+        boolean result = underTest.deleteById(givenId);
+
+        // then
+        verify(repo).deleteById(givenId);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    @DisplayName("should return false if entry to delete does not exist")
+    void should_return_false_if_person_to_delete_does_not_exist() {
+        // given
+        Long givenId = 3L;
+        doReturn(false).when(repo).existsById(givenId);
+
+        // when
+        boolean result = underTest.deleteById(givenId);
+
+        // then
+        verifyNoMoreInteractions(repo);
+        assertThat(result).isFalse();
+    }
+
+}
--- src/test/java/de/htwberlin/webtech/project/TransformEntityTest.java
+++ src/test/java/de/htwberlin/webtech/project/TransformEntityTest.java
@@ -8,166 +8,22 @@ import de.htwberlin.webtech.project.web.Topic;
 import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-
-import java.util.List;
-import java.util.Optional;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.doReturn;
 
+@ExtendWith(MockitoExtension.class)
 public class TransformEntityTest implements WithAssertions {
 
-    private EntryRepository repo = new EntryRepository() {
-        @Override
-        public Optional<EntryEntity> findByTopic(Topic topic) {
-            return Optional.empty();
-        }
+    @Mock
+    private EntryRepository repo;
 
-        @Override
-        public List<EntryEntity> findAll() {
-            return null;
-        }
-
-        @Override
-        public List<EntryEntity> findAll(Sort sort) {
-            return null;
-        }
-
-        @Override
-        public List<EntryEntity> findAllById(Iterable<Long> longs) {
-            return null;
-        }
-
-        @Override
-        public <S extends EntryEntity> List<S> saveAll(Iterable<S> entities) {
-            return null;
-        }
-
-        @Override
-        public void flush() {
-
-        }
-
-        @Override
-        public <S extends EntryEntity> S saveAndFlush(S entity) {
-            return null;
-        }
-
-        @Override
-        public <S extends EntryEntity> List<S> saveAllAndFlush(Iterable<S> entities) {
-            return null;
-        }
-
-        @Override
-        public void deleteAllInBatch(Iterable<EntryEntity> entities) {
-
-        }
-
-        @Override
-        public void deleteAllByIdInBatch(Iterable<Long> longs) {
-
-        }
-
-        @Override
-        public void deleteAllInBatch() {
-
-        }
-
-        @Override
-        public EntryEntity getOne(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public EntryEntity getById(Long aLong) {
-            return null;
-        }
-
-        @Override
-        public <S extends EntryEntity> List<S> findAll(Example<S> example) {
-            return null;
-        }
-
-        @Override
-        public <S extends EntryEntity> List<S> findAll(Example<S> example, Sort sort) {
-            return null;
-        }
-
-        @Override
-        public Page<EntryEntity> findAll(Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends EntryEntity> S save(S entity) {
-            return null;
-        }
-
-        @Override
-        public Optional<EntryEntity> findById(Long aLong) {
-            return Optional.empty();
-        }
-
-        @Override
-        public boolean existsById(Long aLong) {
-            return false;
-        }
-
-        @Override
-        public long count() {
-            return 0;
-        }
-
-        @Override
-        public void deleteById(Long aLong) {
-
-        }
-
-        @Override
-        public void delete(EntryEntity entity) {
-
-        }
-
-        @Override
-        public void deleteAllById(Iterable<? extends Long> longs) {
-
-        }
-
-        @Override
-        public void deleteAll(Iterable<? extends EntryEntity> entities) {
-
-        }
-
-        @Override
-        public void deleteAll() {
-
-        }
-
-        @Override
-        public <S extends EntryEntity> Optional<S> findOne(Example<S> example) {
-            return Optional.empty();
-        }
-
-        @Override
-        public <S extends EntryEntity> Page<S> findAll(Example<S> example, Pageable pageable) {
-            return null;
-        }
-
-        @Override
-        public <S extends EntryEntity> long count(Example<S> example) {
-            return 0;
-        }
-
-        @Override
-        public <S extends EntryEntity> boolean exists(Example<S> example) {
-            return false;
-        }
-    };
-    private final EntryService underTest = new EntryService(this.repo = repo);
+    @InjectMocks
+    private EntryService underTest;
 
     @Test
     @DisplayName("should transform EntryEntity to Entry")
